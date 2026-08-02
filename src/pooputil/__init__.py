e638a72c89c4c505fe231377058d5d2c50c380af
debii <debiiyapp@gmail.com>
2026-08-02 19:08:28 +0700

    Create __init__.py

---
+++ src/pooputil/__init__.py
@@ -0,0 +1,13 @@
+from pooputil.core import (
+    encrypt_target,
+    decrypt_target,
+    secure_delete,
+    is_safe_path
+)
+
+__all__ = [
+    "encrypt_target",
+    "decrypt_target",
+    "secure_delete",
+    "is_safe_path"
+]
